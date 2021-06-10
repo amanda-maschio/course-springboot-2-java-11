637865141e035b08725903a4aa950456393e7397
Amanda Maschio <maschioamanda@hotmail.com>
2021-06-10 15:43:37 -0300

    Service layer, component registration

--- src/main/java/com/cursoudemy/course/resources/UserResource.java
+++ src/main/java/com/cursoudemy/course/resources/UserResource.java
@@ -1,11 +1,16 @@
 package com.cursoudemy.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cursoudemy.course.entities.User;
+import com.cursoudemy.course.services.UserService;
 
 /**
  * Responsável por disponibilizar um recurso web correspondente a entidade User
@@ -16,11 +21,26 @@ import com.cursoudemy.course.entities.User;
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	//AUTOWIRED - o framework fica responsável pela injeção de dependencia ao incluir essa denotação
+	@Autowired
+	private UserService service;
+	
 	//ResponseEntity é um tipo específico do Spring para retornar respostas de requisições web
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+	
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+		
+	}
+	
+	//Indica que a requisição aceita um ID de parâmetro
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+		
 	}
 	
 }
---
+++ src/main/java/com/cursoudemy/course/services/UserService.java
@@ -0,0 +1,32 @@
+package com.cursoudemy.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cursoudemy.course.entities.User;
+import com.cursoudemy.course.repositories.UserRepository;
+
+//@COMPONENT - Registra a classe como componente do Spring
+//@REPOSITORY - Registra um repository
+//@SERVICE - Registra um serviço
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+
+	}
+	
+	public User findById(Long id) {
+		
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
